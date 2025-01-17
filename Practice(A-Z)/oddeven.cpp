367f2d85a3a791588a9f3e73057537cc4dca2238
PrinceTimilsina <timilsinaprince07@gmail.com>
2025-01-17 17:30:43 +0545

    sum

---
+++ Practice(A-Z)/oddeven.cpp
@@ -0,0 +1,15 @@
+//Odd-Even
+#include <iostream>
+using namespace std;
+int main() {
+int a;
+cout<<"Enter a positive number"<<endl;
+cin>>a;
+if(a%2==0){
+    cout<<a<<"is a even number"<<endl;
+}
+else{
+    cout<<a<<"is not a even number"<<endl;
+}
+    return 0;
+}
