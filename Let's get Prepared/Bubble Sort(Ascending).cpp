c5f8f9b0675f07f92f107604e19a9f859efd54f3
PrinceTimilsina <timilsinaprince07@gmail.com>
2025-03-08 08:29:19 +0545

    Create Bubble Sort(Ascending).cpp

---
+++ Let's get Prepared/Bubble Sort(Ascending).cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+class ascending{
+    private:
+    a[10],i,j,temp;
+    public:
+    int getdata(){
+        cout<<"Enter 10 integers:";
+        for(i=0;i<10;i++){
+            cin>>a[i];
+        }
+        return 0;
+    }
+    int sort(){
+        for(i=0;i<10;i++){
+            for(j=0;j<10-1;j++){
+                if(a[j]>n[j+1]){
+                    temp=n[j];
+                    n[j]=n[j+1];
+                    n[j+1]=temp;
+                }
+            }
+        }
+        return 0;
+    }
+int display(){
+       cout<<"Data in ascending order:";
+       for(i=0;i<10;i++){
+        cout<<n[i]<<"  "<<endl;
+           }
+           return 0;
+}
+};
+
+int main(){
+    ascending obj;
+    obj.getdata();
+    obj.sort();
+    obj.display();
+    return 0;
+}
