334f7dc068467d322d4e90a27839046f25a51693
PrinceTimilsina <timilsinaprince07@gmail.com>
2025-03-26 15:17:34 +0545

    new

--- Let's get Prepared/Array1(Even check).cpp
+++ Let's get Prepared/Array1(Even check).cpp
@@ -1,1 +1,18 @@
 //Write a program that allow to insert 10 different integer in an array and calculate the sum of even number.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10], sum = 0;
+
+    cout << "Enter 10 different integers:\n";
+    for (int i = 0; i < 10; i++) {
+        cin >> arr[i];
+        if (arr[i] % 2 == 0) {
+            sum += arr[i];
+        }
+    }
+
+    cout << "Sum of even numbers: " << sum;
+    return 0;
+}
